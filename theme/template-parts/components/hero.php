6d7a348463f0c22b22b7715f856421428120184b
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-02-11 21:58:14 -0500

    convert hero to template part

---
+++ theme/template-parts/components/hero.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Template part for displaying the Hero
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Ramirez_Contractor_LLC
+ */
+
+?>
+
+<!-- TODO: iMAGE DECIDED FROM CUSTOMIZER and default value -->
+
+<section class="hero py-xl-2xl w-full m-0 p-0 bg-cover bg-[url(http://ramirez-contractor.local/wp-content/uploads/2025/01/man-working-steel-fatory-equipment-steel-production-darker.webp)]">
+    <div class="wrapper">
+        <div class="flex flex-wrap justify-evenly gap-xs-s">
+            <div class="hero__text text-light">
+                <p class=" font-display text-[length:var(--wp--preset--font-size--step-6)] "><span class="text-secondary">YOUR MISSION.</span></p>
+                <p class=" font-display text-[length:var(--wp--preset--font-size--step-6)] ">OUR WORKERS</p>
+                <h1 class="font-body text-[length:var(--wp--preset--font-size--step-1)]">Labor Contracting in North Carolina</h1>
+            </div>
+            <form action="" class="hero__form shadow-xl bg-primary p-s-m hidden md:block">
+                <div class="grid">
+                    <div class="flex flex-col">
+                        <label for="name" class="text-light">Name</label>
+                        <input type="text" name="name" id="name">
+                    </div>
+                    <div class="flex flex-col">
+                        <label for="email" class="text-light">Email</label>
+                        <input type="email" name="Email" id="email">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="tel" class="text-light">Phone Number</label>
+                        <input type="tel" name="Phone Number" id="tel">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="company" class="text-light">Company</label>
+                        <input type="text" name="Company Name" id="company">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="workers" class="text-light"># Of Workers</label>
+                        <input type="number" min="1" max="500" name="Number Of Workers" id="workers">
+                    </div>
+
+                    <div class="flex flex-col">
+                        <label for="industry" class="text-light">Industry</label>
+                        <input type="text" name="Industry" id="industry">
+                    </div>
+
+                    <div class="form-message flex flex-col">
+                        <label for="message" class="text-light">Message</label>
+                        <textarea name="" id="message" cols="30" rows="4"></textarea>
+                    </div>
+                    <button type="submit" class="btn col-[2] text-dark bg-tertiary p-2">Submit</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</section>
