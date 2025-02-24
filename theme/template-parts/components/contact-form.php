2760473046a2af92d0f658e8c7dcbdcda1383ba3
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-02-24 16:30:09 -0500

    split contact form from hero

---
+++ theme/template-parts/components/contact-form.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Template part for displaying the contact form
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Ramirez_Contractor_LLC
+ */
+
+?>
+<div class="contact__form bg-primary p-s-m shadow-xl">
+    <form action="" class="grid">
+        <div class="flex flex-col">
+            <label for="name" class="text-light">Name</label>
+            <input type="text" name="name" id="name">
+        </div>
+        <div class="flex flex-col">
+            <label for="email" class="text-light">Email</label>
+            <input type="email" name="Email" id="email">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="tel" class="text-light">Phone Number</label>
+            <input type="tel" name="Phone Number" id="tel">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="company" class="text-light">Company</label>
+            <input type="text" name="Company Name" id="company">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="workers" class="text-light"># Of Workers</label>
+            <input type="number" min="1" max="500" name="Number Of Workers" id="workers">
+        </div>
+
+        <div class="flex flex-col">
+            <label for="industry" class="text-light">Industry</label>
+            <input type="text" name="Industry" id="industry">
+        </div>
+
+        <div class="form-message flex flex-col">
+            <label for="message" class="text-light">Message</label>
+            <textarea name="" id="message" cols="30" rows="4"></textarea>
+        </div>
+        <button type="submit" class="btn bg-tertiary p-2 text-dark md:col-[2]">Submit</button>
+    </form>
+</div>
--- theme/template-parts/components/hero.php
+++ theme/template-parts/components/hero.php
@@ -20,44 +20,7 @@
                 <p class=" font-display text-[length:var(--wp--preset--font-size--step-6)] ">OUR WORKERS</p>
                 <h1 class="font-body text-[length:var(--wp--preset--font-size--step-1)]">Labor Contracting in North Carolina</h1>
             </div>
-            <form action="" class="hero__form hidden bg-primary p-s-m shadow-xl md:block">
-                <div class="grid">
-                    <div class="flex flex-col">
-                        <label for="name" class="text-light">Name</label>
-                        <input type="text" name="name" id="name">
-                    </div>
-                    <div class="flex flex-col">
-                        <label for="email" class="text-light">Email</label>
-                        <input type="email" name="Email" id="email">
-                    </div>
-
-                    <div class="flex flex-col">
-                        <label for="tel" class="text-light">Phone Number</label>
-                        <input type="tel" name="Phone Number" id="tel">
-                    </div>
-
-                    <div class="flex flex-col">
-                        <label for="company" class="text-light">Company</label>
-                        <input type="text" name="Company Name" id="company">
-                    </div>
-
-                    <div class="flex flex-col">
-                        <label for="workers" class="text-light"># Of Workers</label>
-                        <input type="number" min="1" max="500" name="Number Of Workers" id="workers">
-                    </div>
-
-                    <div class="flex flex-col">
-                        <label for="industry" class="text-light">Industry</label>
-                        <input type="text" name="Industry" id="industry">
-                    </div>
-
-                    <div class="form-message flex flex-col">
-                        <label for="message" class="text-light">Message</label>
-                        <textarea name="" id="message" cols="30" rows="4"></textarea>
-                    </div>
-                    <button type="submit" class="btn col-[2] bg-tertiary p-2 text-dark">Submit</button>
-                </div>
-            </form>
+            <?php get_template_part("template-parts/components/contact-form"); ?>
         </div>
     </div>
 </section>
