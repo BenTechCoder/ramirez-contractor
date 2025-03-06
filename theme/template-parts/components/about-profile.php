f2ddf637c6d7e5799f10db84c18a2f5e49d1f31e
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-03-06 11:36:35 -0500

    added about cards

---
+++ theme/template-parts/components/about-profile.php
@@ -0,0 +1,9 @@
+<article class="profile switcher items-center cta-pattern bg-primary text-light">
+    <?php echo $args['image'] ?>
+    <div class="stack self-center p-xl-2xl bg-dark text-light">
+        <h3 class="underline text-light">
+            <?php echo $args['name']; ?>
+        </h3>
+        <?php the_content() ?>
+    </div>
+</article>
