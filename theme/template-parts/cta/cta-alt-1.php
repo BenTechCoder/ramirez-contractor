4826759b67edc1234981ff4d3baf095ba4943d68
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-03-18 21:09:51 -0400

    added 2 cta variations

---
+++ theme/template-parts/cta/cta-alt-1.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Template part for displaying the main CTA
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Ramirez_Contractor_LLC
+ */
+
+?>
+
+<section class="cta cta-pattern bg-tertiary py-xl-2xl">
+    <div class="wrapper switcher items-center justify-center" style="--switcher-space: var(--wp--preset--spacing--xl)">
+        <div class="stack">
+            <h2>Skilled Industrial Labor on Demand.</h2>
+            <p>Send Ramirez Contractor a Message and let us find the perfect people for your needs </p>
+            <div>
+                <?php
+                if (is_front_page()) {
+                    echo '<a href="/contact/" class="btn bg-primary text-light">Request Workers</a>';
+                }
+                ?>
+                <?php
+                if (!is_page('faq')) {
+                    echo '<a href="/faq/" class="btn bg-primary text-light">FAQ</a>';
+                }
+                ?>
+            </div>
+        </div>
+        <?php
+        if (!is_front_page()) {
+            get_template_part("template-parts/components/contact-form");
+        }
+        ?>
+    </div>
+</section>
---
+++ theme/template-parts/cta/cta-alt-2.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Template part for displaying the main CTA
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package Ramirez_Contractor_LLC
+ */
+
+?>
+
+<section class="cta bg-tertiary bg-center py-2xl-3xl" style="background-image:url(<?php echo get_parent_theme_file_uri("/assets/images/pexels-tiger-lily-4483777.webp") ?>);">
+    <div class="wrapper switcher center items-center justify-center text-center text-light" style="--switcher-space: var(--wp--preset--spacing--xl);">
+        <div class="stack">
+            <h2>More Than a Job Well Done</h2>
+            <p>Send Ramirez Contractor a Message and let us find the perfect people for your needs </p>
+            <div>
+                <?php
+                if (!is_page('contact')) {
+                    echo '<a href="/contact/" class="btn bg-primary text-light">Request Workers</a>';
+                }
+                ?>
+                <?php
+                if (!is_page('faq')) {
+                    echo '<a href="/faq/" class="btn bg-primary text-light">FAQ</a>';
+                }
+                ?>
+            </div>
+        </div>
+    </div>
+</section>
