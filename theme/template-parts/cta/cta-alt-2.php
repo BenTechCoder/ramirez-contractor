2d2bf2ee3820325d76cb920acb3ba450c15e01e6
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-03-19 11:00:38 -0400

    removed switcher form cta alt 2

--- theme/template-parts/cta/cta-alt-2.php
+++ theme/template-parts/cta/cta-alt-2.php
@@ -11,7 +11,7 @@
 ?>
 
 <section class="cta bg-tertiary bg-center py-2xl-3xl" style="background-image:url(<?php echo get_parent_theme_file_uri("/assets/images/pexels-tiger-lily-4483777.webp") ?>);">
-    <div class="wrapper switcher center items-center justify-center text-center text-light" style="--switcher-space: var(--wp--preset--spacing--xl);">
+    <div class="wrapper center items-center justify-center text-center text-light" style="--switcher-space: var(--wp--preset--spacing--xl);">
         <div class="stack">
             <h2>More Than a Job Well Done</h2>
             <p>Send Ramirez Contractor a Message and let us find the perfect people for your needs </p>
