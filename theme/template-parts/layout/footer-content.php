5f58a8093dbea86ba0e63b25f104575008774162
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-03-19 11:00:48 -0400

    remove bloginfo title from footer

--- theme/template-parts/layout/footer-content.php
+++ theme/template-parts/layout/footer-content.php
@@ -32,15 +32,6 @@
 		<?php endif; ?>
 
 		<!-- 
-		<?php
-		$ramirez_contractor_blog_info = get_bloginfo('name');
-		if (! empty($ramirez_contractor_blog_info)) :
-		?>
-			<a href="<?php echo esc_url(home_url('/')); ?>" rel="home"><?php bloginfo('name'); ?></a>
-		<?php
-		endif;
-
-		?> -->
 	</div>
 	</section>
 
