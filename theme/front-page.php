5962e9c284a92b19a2cc24699bd4a17b0da5af5f
Benjamin Lebron <71533854+BenTechCoder@users.noreply.github.com>
2025-04-17 15:25:08 -0400

    move services-cta to component

--- theme/front-page.php
+++ theme/front-page.php
@@ -10,7 +10,6 @@
 
 /**
  * ⌛ get google maps embed for the service area 
-  TODO: Get component ready for testimonials/companies worked with
   TODO: add customizer for Front Page text
   TODO: Look into load-in animations with css
  */
@@ -20,26 +19,7 @@ get_header();
 
 <?php get_template_part('template-parts/components/hero'); ?>
 
-
-
-<section class="stack py-s-m">
-    <div class="wrapper stack center text-center" style="--space: var(--wp--preset--spacing--s)">
-        <p class="text-primary">Services</p>
-        <h2>Finding the right people for your mission</h2>
-        <p>Etiam fermentum metus vitae maximus fringilla. Sed non tellus pharetra, vestibulum velit et, fringilla ante. Donec suscipit libero nisl, eget tempor erat tincidunt et. Fusce molestie elit tincidunt, malesuada massa nec, placerat ex. Nulla luctus ante gravida mauris vehicula finibus. </p>
-
-        <?php get_template_part('template-parts/assets/services-divider', '', array('rotate' => false)); ?>
-
-    </div>
-    <div class="bg-contain bg-no-repeat md:bg-cover" style="background-image:url(<?php echo get_parent_theme_file_uri("/assets/svg/services-background.svg") ?>)">
-
-        <?php get_template_part('/template-parts/components/services-grid'); ?>
-
-
-        <div class=" stack wrapper center"> <?php get_template_part('template-parts/assets/services-divider', '', array('rotate' => true)); ?></div>
-    </div>
-
-</section>
+<?php get_template_part('template-parts/components/services-cta'); ?>
 
 <?php get_template_part('template-parts/components/testimonials'); ?>
 
---
+++ theme/template-parts/components/services-cta.php
@@ -0,0 +1,18 @@
+<section class="stack py-s-m">
+    <div class="wrapper stack center text-center" style="--space: var(--wp--preset--spacing--s)">
+        <p class="text-primary">Services</p>
+        <h2>Finding the right people for your mission</h2>
+        <p>Ramirez Contractor LLC specializes in providing tailored labor solutions to meet your business needs. From skilled workers to general labor, we connect you with reliable professionals who get the job done right.</p>
+
+        <?php get_template_part('template-parts/assets/services-divider', '', array('rotate' => false)); ?>
+
+    </div>
+    <div class="bg-contain bg-no-repeat md:bg-cover" style="background-image:url(<?php echo get_parent_theme_file_uri("/assets/svg/services-background.svg") ?>)">
+
+        <?php get_template_part('/template-parts/components/services-grid'); ?>
+
+
+        <div class=" stack wrapper center"> <?php get_template_part('template-parts/assets/services-divider', '', array('rotate' => true)); ?></div>
+    </div>
+
+</section>
